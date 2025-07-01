3e651217524f9ffe750070e8c569146c91557fbb
o_rixsimboyev <o.rixsimboyev@uzinfocom.uz>
2025-07-01 18:05:41 +0500

    auth styles

--- src/app/layouts/views/auth-layout/auth-layout.ts
+++ src/app/layouts/views/auth-layout/auth-layout.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { Logo } from '@shared/components';
 
 @Component({
   selector: 'cpd-auth-layout',
-  imports: [Logo],
+  imports: [Logo, RouterOutlet],
   templateUrl: './auth-layout.html',
   styleUrl: './auth-layout.less',
   changeDetection: ChangeDetectionStrategy.OnPush,
--- src/app/pages/auth/pages/login/login.ts
+++ src/app/pages/auth/pages/login/login.ts
@@ -1,12 +1,22 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TuiInputModule, TuiTextfieldControllerModule } from '@taiga-ui/legacy';
+import { TuiIcon } from '@taiga-ui/core';
+import { TuiButton } from '@taiga-ui/core';
+import { TuiLink } from '@taiga-ui/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'cpd-login',
-  imports: [],
+  imports: [
+    TuiInputModule,
+    TuiIcon,
+    TuiTextfieldControllerModule,
+    TuiButton,
+    TuiLink,
+    RouterLink,
+  ],
   templateUrl: './login.html',
   styleUrl: './login.less',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Login {
-
-}
+export class Login {}
